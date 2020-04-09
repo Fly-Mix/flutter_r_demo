f72b9f883465081438cae18b5e03c2ce77c7fc17
York <yorkzhang520@gmail.com>
2020-04-09 22:44:03 +0800

    change: update ios example config

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/York/Workspace/Flutterspace/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/York/Workspace/GitHub/fly_flutter_r_demo"
-export "FLUTTER_TARGET=/Users/York/Workspace/GitHub/fly_flutter_r_demo/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/York/Workspace/GitHub/flutter_r_demo"
+export "FLUTTER_TARGET=/Users/York/Workspace/GitHub/flutter_r_demo/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/York/Workspace/Flutterspace/flutter/bin/cache/artifacts/engine/ios"
